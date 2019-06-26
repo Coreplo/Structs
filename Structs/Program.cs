afcf8908dfc9deeee74781cec6b877903e751e89
Coreplo <coreplo@gmail.com>
2019-06-26 19:21:20 +0200

    learning structs in c siarp

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -6,16 +6,52 @@ using System.Threading.Tasks;
 
 namespace Structs
 {
+    //Struct
+    //a struct is a value type that is tipically used to incapsulate small
+    //groups of related variables
+    //they dont need the new keyword to be instantiated
+    //they dont support inheritance
+    //they cannot contain virtual methods
+    //they can have some methods eg vector3 in unity has magnitude or normalized method
+    //also can contain indexes and properties
+    //they cannot contain default constructors but they can have contructors that take parameters
+    //in that case they need the "new" keyword
+
+    // hint: all c#types are structs: int, double, bool etc...
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Book b;
+            b.title = "Cars and Farts";
+            b.price = 19.90;
+            b.author = "Gass Mann";
+            Console.WriteLine(b.title);
+
+            Point p = new Point(1, 2);
+            Console.WriteLine(p.x);
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+
+    struct Book
+    {
+        public string title;
+        public double price;
+        public string author;
+    }
+
+    struct Point
+    {
+        public int x;
+        public int y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
         }
     }
 }
